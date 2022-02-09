2501d4273310d5cec9ca90e1bb12c79bcc2ba5c0
jemin <jemin03120111@gmail.com>
2022-02-10 00:00:13 +0900

    interface proxy

---
+++ src/main/java/hello/proxy/config/v1_proxy/interface_proxy/OrderControllerInterfaceProxy.java
@@ -0,0 +1,38 @@
+package hello.proxy.config.v1_proxy.interface_proxy;
+
+import hello.proxy.app.v1.OrderControllerV1;
+import hello.proxy.app.v1.OrderRepositoryV1;
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RequiredArgsConstructor
+@Slf4j
+public class OrderControllerInterfaceProxy implements OrderControllerV1 {
+
+    private final OrderControllerV1 target;  // 실제 객체
+    private final LogTrace logTrace;
+
+    @Override
+    public String request(String itemId) {
+        TraceStatus status = null;
+        try {
+            status =  logTrace.begin("OrderController.request()");
+            //target 호출
+            String result = target.request(itemId);
+            logTrace.end(status);
+            return result;
+        }
+        catch (Exception e)
+        {
+            logTrace.exception(status,e);
+            throw e;
+        }
+    }
+
+    @Override
+    public String noLog() {
+        return target.noLog(); // 단순 위임
+    }
+}
--- src/test/java/hello/proxy/decorator/DecoratorPatternTest.java
+++ src/test/java/hello/proxy/decorator/DecoratorPatternTest.java
@@ -1,7 +1,6 @@
 package hello.proxy.decorator;
 
-import hello.proxy.decorator.code.DecoratorPatternClient;
-import hello.proxy.decorator.code.RealComponent;
+import hello.proxy.decorator.code.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +11,25 @@ public class DecoratorPatternTest {
     void noDecorator(){
         RealComponent realComponent = new RealComponent();
         DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+        client.execute();
+    }
+
+    @Test
+    void decorator1()
+    {
+        Component realComponent = new RealComponent();
+        MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+        client.execute();
+    }
+
+    @Test
+    void decorator2()
+    {
+        Component realComponent = new RealComponent();
+        MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+        Component timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
         client.execute();
     }
 }
